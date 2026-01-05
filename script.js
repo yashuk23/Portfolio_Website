336d6cdb75b8b5b45d369ffefc9b3ea986d2c1cc
Akhil Kanawade <yashkanawade66@gmail.com>
2026-01-05 23:02:32 +0530

    Initial commit: basic portfolio structure

---
+++ script.js
@@ -0,0 +1,6 @@
+let download_resume=document.querySelector(".hero .hero-text .hero-text-content .resume-button");
+download_resume.addEventListener("click",()=>
+{
+    console.log("button clicked");
+    window.open("https://drive.google.com/file/d/1iMCd3pnco5XrdyBPANwymZ2LkqTi-Ffs/view?usp=drive_link","_BLANK")
+})

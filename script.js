d21ff3b53cf8579b38917e5645d96ad8b8f03d72
Akhil Kanawade <yashkanawade66@gmail.com>
2026-01-07 15:24:04 +0530

    Test GitHub Pages deployment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let download_resume=document.querySelector(".hero .hero-text .hero-text-content .resume-button");
 download_resume.addEventListener("click",()=>
 {
-    console.log("button clicked");
     window.open("https://drive.google.com/file/d/1iMCd3pnco5XrdyBPANwymZ2LkqTi-Ffs/view?usp=drive_link","_BLANK")
+})
+
+let leetcode=document.querySelector('.coding-profiles .coding-profiles-btns #leetcode');
+leetcode.addEventListener("click",()=>
+{
+    window.open("https://leetcode.com/u/yashuk_23/");
+})
+
+let tuf=document.querySelector('.coding-profiles .coding-profiles-btns #tuf');
+tuf.addEventListener("click",()=>
+{
+    window.open("https://takeuforward.org/profile/yashuk23");
+})
+
+let gfg=document.querySelector('.coding-profiles .coding-profiles-btns #gfg');
+gfg.addEventListener("click",()=>
+{
+    window.open("https://www.geeksforgeeks.org/profile/yashuk23?tab=activity");
 })
